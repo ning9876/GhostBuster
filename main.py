6dedd69cfde49f08ea3994c8ab7ee1572d0cc1f2
ning9876 <ning_zheng@dlsu.edu.ph>
2022-11-22 15:45:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+import socket
+from os import system, name, path
+import subprocess
+#Pydictor author: LandGrey
+from __future__ import unicode_literals
+from lib.fun.fun import cool
+from lib.fun.decorator import magic
+from lib.data.data import pyoptions
+
